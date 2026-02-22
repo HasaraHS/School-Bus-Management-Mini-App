cfb73906f9ee567d3c1ae7b4de050df9687430a0
IT22342058 <IT22342058@my.sliit.lk>
2026-02-22 06:51:18 +0530

    server running

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
